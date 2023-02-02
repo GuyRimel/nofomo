737b650b47034ecfcfea81557484dffd353e8961
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-02-02 09:39:20 -0600

    fixed a nesting bug with recharts

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React, { Component } from 'react';
-import { ResponsiveContainer, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
-import './App.css';
-import './nprogress.css';
+import React, { Component } from "react";
+import {
+  ResponsiveContainer,
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+import "./App.css";
+import "./nprogress.css";
 // COMPONENTS //////////
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import EventList from './EventList';
-import TopBar from './TopBar';
-import WelcomeScreen from './WelcomeScreen';
-import EventGenre from './EventGenre';
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import EventList from "./EventList";
+import TopBar from "./TopBar";
+import WelcomeScreen from "./WelcomeScreen";
+import EventGenre from "./EventGenre";
 // DATA / FUNCS //////////
-import { getEvents, extractLocations, checkToken, getAccessToken } from
-'./api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    seletedLocation: 'all',
+    seletedLocation: "all",
     eventCount: 32,
-    showWelcomeScreen: undefined
+    showWelcomeScreen: undefined,
     // infoText: ''
-  }
-  
+  };
+
   async componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
+    const accessToken = localStorage.getItem("access_token");
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-    
+
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
 
     if ((code || isTokenValid) && this.mounted) {
@@ -41,57 +48,61 @@ class App extends Component {
     }
   }
 
-  componentWillUnmount(){ this.mounted = false; }
-
-  updateEvents = (location, inputNumber) => {
-    const {eventCount, seletedLocation} = this.state;
-    if (location) {
-      getEvents().then((events) => {
-        const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-        const eventsToShow=locationEvents.slice(0, eventCount);
-        this.setState({
-        events: eventsToShow,
-        seletedLocation: location
-        });
-      });  
-    } else {
-      getEvents().then((events) => {
-        const locationEvents = (seletedLocation === 'all') ?
-        events :
-        events.filter((event) => event.location === seletedLocation);
-        const eventsToShow=locationEvents.slice(0, inputNumber);
-        this.setState({
-          events: eventsToShow,
-          eventCount: inputNumber
-        });
-      })
-    }
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
+  updateEvents = (location, inputNumber) => {
+    const { eventCount, seletedLocation } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents =
+          location === "all"
+            ? events
+            : events.filter((event) => event.location === location);
+        const eventsToShow = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: eventsToShow,
+          seletedLocation: location,
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents =
+          seletedLocation === "all"
+            ? events
+            : events.filter((event) => event.location === seletedLocation);
+        const eventsToShow = locationEvents.slice(0, inputNumber);
+        this.setState({
+          events: eventsToShow,
+          eventCount: inputNumber,
+        });
+      });
+    }
+  };
+
   getData = () => {
-    const {locations, events} = this.state;
-    const data = locations.map((location)=>{
-      const number = events.filter((event) => event.location === location).length
-      const city = location.split(', ').shift()
-      return {city, number};
-    })
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
     return data;
   };
 
   render() {
-    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
 
     const { locations, eventCount, events, showWelcomeScreen } = this.state;
     return (
       <div className="App">
         <TopBar />
         <div className="filter-box">
-          <CitySearch
-            locations={locations}
-            updateEvents= {this.updateEvents}
-          />
+          <CitySearch locations={locations} updateEvents={this.updateEvents} />
           <NumberOfEvents
             eventCount={eventCount}
             updateEvents={this.updateEvents}
@@ -99,33 +110,37 @@ class App extends Component {
         </div>
         <h4 className="text-center text-splash">Events by City</h4>
 
-        <div className='data-vis-wrapper'>
-          <ResponsiveContainer height={400} >
-            <EventGenre events={events} />
-            <ScatterChart
-              margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-            >
+        <div className="data-vis-wrapper">
+          <EventGenre events={events} />
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="city" stroke='white' />
+              <XAxis
+                type="category"
+                dataKey="city"
+                name="city"
+                stroke="white"
+              />
               <YAxis
-                stroke='white'
+                stroke="white"
                 allowDecimals={false}
                 type="number"
                 dataKey="number"
                 name="number of events"
               />
-              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter data={this.getData()} fill={`hsl(190, 100%, 90%)`} />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="hsl(190, 100%, 90%)" />
             </ScatterChart>
           </ResponsiveContainer>
         </div>
 
-
         <EventList events={events} />
         <WelcomeScreen
-          showWelcomeScreen={showWelcomeScreen}
-          // showWelcomeScreen={false}
-          getAccessToken={() => { getAccessToken() }}
+          // showWelcomeScreen={showWelcomeScreen}
+          showWelcomeScreen={false}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
         />
       </div>
     );
--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,44 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import {
-  PieChart, Pie, ResponsiveContainer
-} from 'recharts';
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
-  useEffect(() => { 
+  useEffect(() => {
     const getData = () => {
+      const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
 
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) =>
+          event.summary.split(" ").includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
+    setData(() => getData());
+  }, [events]);
 
-        const data = genres.map(genre => {
-            const value = events.filter(event => event.summary.split(' ').includes(genre)).length
-            return { name: genre, value };
-        })
-        return data;
-      };
-    setData(() => getData());}, [events]);
-
-
-    
   return (
-    <ResponsiveContainer className="pieChart" height={400}>
+    <ResponsiveContainer className="EventGenre__pie-chart" height={400}>
       <PieChart width={400} height={400}>
         <Pie
-        stroke="#fff"
-        data={data}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        outerRadius={80}
-        fill={`hsl(190, 100%, 66%)`}
-        dataKey="value"
-        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-        >
-        </Pie>
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          outerRadius={80}
+          fill="hsl(190, 100%, 90%)"
+          dataKey="value"
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
+        ></Pie>
       </PieChart>
     </ResponsiveContainer>
   );
-}
+};
 
-export default EventGenre
+export default EventGenre;
