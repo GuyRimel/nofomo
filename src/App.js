4ee626df4e430ca2fdb56c40a0df32e53fe62f0b
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-02-02 13:48:50 -0600

    finalizing App.js

--- src/App.js
+++ src/App.js
@@ -179,8 +179,7 @@ class App extends Component {
 
         <EventList events={events} />
         <WelcomeScreen
-          // showWelcomeScreen={showWelcomeScreen}
-          showWelcomeScreen={false}
+          showWelcomeScreen={showWelcomeScreen}
           getAccessToken={() => {
             getAccessToken();
           }}
